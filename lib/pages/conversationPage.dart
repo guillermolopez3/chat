543a289f11a76cd8085fc7350063165fc0bf80c5
Guille <guillermolopez3@gmail.com>
2019-09-15 20:19:33 -0300

    diseño de bottom sheet

--- lib/pages/conversationPage.dart
+++ lib/pages/conversationPage.dart
@@ -1,4 +1,5 @@
 import 'package:chat/widgets/chatListWidget.dart';
+import 'package:chat/widgets/conversationBottomSheet.dart';
 import 'package:chat/widgets/inputWidget.dart';
 import 'package:flutter/material.dart';
 import '../widgets/chatAppBar.dart';
@@ -9,21 +10,28 @@ class ConversationPage extends StatefulWidget {
 }
 
 class _ConversationPageState extends State<ConversationPage> {
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>(); //sirve para mostrar el botonsheet modal
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        key: _scaffoldKey,
         appBar: ChatAppBar(),
         body: Stack(
           children: <Widget>[
             Column(
               children: <Widget>[
                 ChatListWidget(),
+                //onPanUpdate devuelve el gesto del usuario, dy>0 swipe up, dy<0 swipe down
                 GestureDetector(
                   child: InputWidget(),
                   onPanUpdate: (details){
                     if(details.delta.dy < 0){
-
+                      _scaffoldKey.currentState.showBottomSheet<Null>((BuildContext context){
+                        return ConversationBottomSheet();
+                      });
                     }
                   },
                 )
---
+++ lib/widgets/conversationBottomSheet.dart
@@ -0,0 +1,39 @@
+import 'package:chat/config/palette.dart';
+import 'package:chat/config/style.dart';
+import 'package:chat/widgets/chatRowWidget.dart';
+import 'package:chat/widgets/navigationPillWidget.dart';
+import 'package:flutter/material.dart';
+
+class ConversationBottomSheet extends StatefulWidget {
+  @override
+  _ConversationBottomSheetState createState() => _ConversationBottomSheetState();
+}
+
+class _ConversationBottomSheetState extends State<ConversationBottomSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            body: ListView(children: <Widget>[
+              NavigationPillWidget(),
+              Center(child: Text('Messages', style: Styles.textHeading)),
+              SizedBox(
+                height: 20,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemCount: 5,
+                separatorBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.only(left: 75,right: 20),
+                    child: Divider(
+                      color: Palette.accentColor,
+                    )),
+                itemBuilder: (context, index) {
+                  return ChatRowWidget();
+                },
+              )
+            ])));
+  }
+}
